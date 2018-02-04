74f4dd42a037cc9213e11c01185852bb5424cb5b
ScottGrogin <israelgrogin@gmail.com>
2018-02-04 01:34:49 -0800

    Added page controls and borders

--- AliceSite/js/variRot.js
+++ AliceSite/js/variRot.js
@@ -1,3 +1,5 @@
+var currentPage = 0;
+
 function rotateNav() {
     var navs = document.querySelectorAll("nav li");
 
@@ -7,21 +9,64 @@ function rotateNav() {
         navs[i].style.WebkitTransform = "rotate(" + rando + "deg)";
         navs[i].style.msTransform = "rotate(" + rando + "deg)";
         navs[i].style.transform = "rotate(" + rando + "deg)";
+        gotoPage(3);
     }
-    pageTurner(0);
 }
 
-function pageTurner(currentPage) {
-    
+function pageTurner(increment) {
+    currentPage += increment;
     var pages = document.getElementsByClassName("page");
+    if (currentPage > pages.length - 1) {
+        currentPage = 0;
+    }
+    if (currentPage < 0) {
+        currentPage = pages.length - 1;
+    }
 
-    
+
     for (var i = 0; i < pages.length; i++) {
         pages[i].style.display = "none";
 
     }
-    pages[currentPage].style.display ="block";
+    pages[currentPage].style.display = "block";
 
+}
+window.onkeydown = function pageTurnerKey(e) {
+    var keyCode = e.keyCode || e.which;
+    if (keyCode == 37) {
+        currentPage -= 1;
+    }
+    if (keyCode == 39) {
+        currentPage += 1;
+    }
+
+    var pages = document.getElementsByClassName("page");
+    if (currentPage > pages.length - 1) {
+        currentPage = 0;
+    }
+    if (currentPage < 0) {
+        currentPage = pages.length - 1;
+    }
+
+
+    for (var i = 0; i < pages.length; i++) {
+        pages[i].style.display = "none";
+
+    }
+    pages[currentPage].style.display = "block";
+
+}
+
+function gotoPage(pageNum) {
+    currentPage = pageNum;
+    var pages = document.getElementsByClassName("page");
+
+
+    for (var i = 0; i < pages.length; i++) {
+        pages[i].style.display = "none";
+
+    }
+    pages[currentPage].style.display = "block";
 }
 
 window.onload = rotateNav;
